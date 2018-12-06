c50a48b331f6307bef8cba70a31e4da10638dae4
AsafDavidov <adavidov@conncoll.edu>
2018-12-06 08:51:43 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,14 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+  function dispatch(action){
+    state = reducer(state,action)
+    render()
+  }
+  function getState(){
+    return reducer(state,"@@INIT")
+  }
+  return {dispatch,getState}
 }
 
 function render() {
